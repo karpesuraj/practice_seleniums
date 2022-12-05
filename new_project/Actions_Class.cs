14098a7af1a05441a7c96d3fb3ad6b52d92b8c97
unknown <surajkarpe2879@gmail.com>
2022-12-05 12:35:17 +0530

    comments

---
+++ new_project/Actions_Class.cs
@@ -0,0 +1,15 @@
+﻿//using OpenQA.Selenium;
+//using System.Collections;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Interactions;
+
+//IWebDriver Driver = new ChromeDriver();
+//Driver.Navigate().GoToUrl("https://www.payback.in/");
+//Actions ac = new Actions(Driver);
+//Driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+//ac.MoveToElement(Driver.FindElement(By.XPath("//*[@id=\"myUserbar\"]/ul[1]/li/a"))).Click().Build().Perform();
+//Driver.FindElement(By.XPath("//input[@placeholder='PAYBACK Card Number / Mobile Number']")).SendKeys("9637479256");
+//Driver.FindElement(By.XPath("//input[@placeholder='4-Digit PIN']")).SendKeys("2879");
+//Thread.Sleep(10000);
+//Driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div/div/div/div/div[1]/div[1]/section/form/button[2]")).Click();
+
---
+++ new_project/Frame_Handling.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_project
+{
+    internal class Frame_Handling
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://netbanking.hdfcbank.com/netbanking/");
+            Thread.Sleep(3000);
+            Driver.SwitchTo().Frame(0);
+            Driver.FindElement(By.XPath("//*[@id=\"pageBody\"]/div[1]/form/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/input")).SendKeys("1245");
+            Driver.FindElement(By.XPath("//*[@id=\"pageBody\"]/div[1]/form/div[3]/div/div/div[2]/div[2]/div[2]/div[2]/a")).Click();
+            Thread.Sleep(9000);
+            Driver.Quit();
+        }
+     
+    }
+}
